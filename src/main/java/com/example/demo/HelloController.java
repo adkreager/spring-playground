1b483f5db5f8507f46c8a535ba71a026519c12f2
Alexis <alexisdkreager@gmail.com>
2020-07-20 11:50:37 -0500

    /math/calculate

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +13,30 @@ public class HelloController {
     }
 
     @GetMapping("/math/pi")
-        public String returnPi() {
+    public String returnPi() {
         return "3.141592653589793";
     }
 
+    @GetMapping("/math/calculate")
+    public String returnOperation(
+        @RequestParam("operation") String operator,
+        @RequestParam("x") int x,
+        @RequestParam("y") int y) {
+        switch(operator) {
+            case "add":
+                return x + " + " + y + " = " + (x + y);
+            case "subtract":
+                return x + " - " + y + " = " + (x - y);
+            case "multiply":
+                return x + " * " + y + " = " + (x * y);
+            case "divide":
+                if (y != 0) {
+                    return x + " / " + y + " = " + (x / y);
+                } else {
+                    return "Error";
+                }
+            default:
+                return "Enter a valid operation type";
+        }
+    }
 }
