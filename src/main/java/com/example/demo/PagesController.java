597c6b0891f6f63fb32abf36aef32c21b37c7ee1
Alexis <alexisdkreager@gmail.com>
2020-07-22 11:11:54 -0500

    Update PagesController.java

--- src/main/java/com/example/demo/PagesController.java
+++ src/main/java/com/example/demo/PagesController.java
@@ -23,38 +23,38 @@ public class PagesController {
         flight.setTickets(tickets);
         return flight;
     }
-    @GetMapping("/flights")
-    public Flight[] getFlights() {
-        Flight[] flights = new Flight[2];
-        flights[0] = new Flight();
-        flights[0].setDeparts(new Date());
-        Flight.Ticket[] tickets1 = new Flight.Ticket[1];
-        Flight.Ticket.Person person1 = new Flight.Ticket.Person();
-        person1.setFirstName("Some name");
-        person1.setLastName("Some other name");
-        tickets1[0] = new Flight.Ticket();
-        tickets1[0].setPassenger(person1);
-        tickets1[0].setPrice(200);
-        flights[0].setTickets(tickets1);
-        flights[1] = new Flight();
-        flights[1].setDeparts(new Date());
-        Flight.Ticket[] tickets2 = new Flight.Ticket[1];
-        Flight.Ticket.Person person2 = new Flight.Ticket.Person();
-        person2.setLastName("Some other name");
-        tickets2[0] = new Flight.Ticket();
-        tickets2[0].setPassenger(person2);
-        tickets2[0].setPrice(400);
-        flights[1].setTickets(tickets2);
-        return flights;
-    }
-    @PostMapping("flights/tickets/total")
-    public Map<String, Integer> sumTicketsTotal(@RequestBody() Flight flight) {
-        int total = 0;
-        for (Flight.Ticket ticket : flight.getTickets()) {
-            total += ticket.getPrice();
-        }
-        Map<String, Integer> result = new HashMap<>();
-        result.put("result", total);
-        return result;
-    }
+//    @GetMapping("/flights")
+//    public Flight[] getFlights() {
+//        Flight[] flights = new Flight[2];
+//        flights[0] = new Flight();
+//        flights[0].setDeparts(new Date());
+//        Flight.Ticket[] tickets1 = new Flight.Ticket[1];
+//        Flight.Ticket.Person person1 = new Flight.Ticket.Person();
+//        person1.setFirstName("Some name");
+//        person1.setLastName("Some other name");
+//        tickets1[0] = new Flight.Ticket();
+//        tickets1[0].setPassenger(person1);
+//        tickets1[0].setPrice(200);
+//        flights[0].setTickets(tickets1);
+//        flights[1] = new Flight();
+//        flights[1].setDeparts(new Date());
+//        Flight.Ticket[] tickets2 = new Flight.Ticket[1];
+//        Flight.Ticket.Person person2 = new Flight.Ticket.Person();
+//        person2.setLastName("Some other name");
+//        tickets2[0] = new Flight.Ticket();
+//        tickets2[0].setPassenger(person2);
+//        tickets2[0].setPrice(400);
+//        flights[1].setTickets(tickets2);
+//        return flights;
+//    }
+//    @PostMapping("flights/tickets/total")
+//    public Map<String, Integer> sumTicketsTotal(@RequestBody() Flight flight) {
+//        int total = 0;
+//        for (Flight.Ticket ticket : flight.getTickets()) {
+//            total += ticket.getPrice();
+//        }
+//        Map<String, Integer> result = new HashMap<>();
+//        result.put("result", total);
+//        return result;
+//    }
 }
