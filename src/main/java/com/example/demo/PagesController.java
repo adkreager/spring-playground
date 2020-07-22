0af8c7479d2f4a12e2fa396d5f9c44aaa345425d
Alexis <alexisdkreager@gmail.com>
2020-07-22 08:23:40 -0500

    PagesController will not bootRun

--- src/main/java/com/example/demo/PagesController.java
+++ src/main/java/com/example/demo/PagesController.java
@@ -1,9 +1,6 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.HashMap;
